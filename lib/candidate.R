00bf9bc83299ed1201fbddffa48af57a6c3e2cc8
Shilin Li <sl4261@columbia.edu>
2018-11-27 15:11:50 -0500

    Update lib/candidate.R

--- lib/candidate.R
+++ lib/candidate.R
@@ -9,6 +9,15 @@ library(stringi)
 candidate <- function(my_word){
   list_candidate <- list(NA)
   
+  # insertion
+  list_word <- list(NA)
+  for (i in 1:nchar(my_word)) {
+    list_word[[i]] <- my_word
+    stri_sub(list_word[[i]],i,i) <- ""
+  }
+  vec_word_insert <- unlist(list_word)
+  insert_candidate <- vec_word_insert[!is.na(match(vec_word_insert,unlist(tesseract_vec_100)) > 0)]
+  
   # Deletion
   list_word <- list(NA)
   list_word[[1]] <- paste0(letters,my_word)
@@ -21,21 +30,12 @@ candidate <- function(my_word){
   
   # substitution
   list_word <- list(NA)
-  for (j in 1:nchar(my_word)) {
-    list_word[[j]] <- my_word
-    stri_sub(list_word[[j]],j,j) <- letters
+  for (i in 1:nchar(my_word)) {
+    list_word[[i]] <- my_word
+    stri_sub(list_word[[i]],i,i) <- letters
   }
   vec_word_substitute <- unlist(list_word)
   substitute_candidate <- vec_word_substitute[!is.na(match(vec_word_substitute,unlist(tesseract_vec_100)) > 0)]
-  
-  # insertion
-  list_word <- list(NA)
-  for (i in 1:nchar(my_word)) {
-    list_word[[j]] <- my_word
-    stri_sub(list_word[[i]],i,i) <- ""
-  }
-  vec_word_insert <- unlist(list_word)
-  insert_candidate <- vec_word_insert[!is.na(match(vec_word_insert,unlist(tesseract_vec_100)) > 0)]
   
   # reversal
   list_word <- list(NA)
@@ -47,10 +47,9 @@ candidate <- function(my_word){
   vec_word_reversal <- unlist(list_word)
   reversal_candidate <- vec_word_reversal[!is.na(match(vec_word_reversal,unlist(tesseract_vec_100)) > 0)]
   
-  
-  list_candidate[[1]] <- unique(delete_candidate)
-  list_candidate[[2]] <- unique(substitute_candidate)
-  list_candidate[[3]] <- unique(insert_candidate)
+  list_candidate[[1]] <- unique(insert_candidate)
+  list_candidate[[2]] <- unique(delete_candidate)
+  list_candidate[[3]] <- unique(substitute_candidate)
   list_candidate[[4]] <- unique(reversal_candidate)
   
   return(list_candidate)
