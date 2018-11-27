622829d7dc26f8b74e0700417fb942bec35595ae
Shilin Li <sl4261@columbia.edu>
2018-11-27 15:04:23 -0500

    Update lib/candidate.R

--- lib/candidate.R
+++ lib/candidate.R
@@ -17,7 +17,7 @@ candidate <- function(my_word){
     stri_sub(list_word[[i]],i,i-1) <- letters
   }
   vec_word_delete <- unlist(list_word)
-  delete_candidate <- vec_word_delete[!is.na(match(vec_word_delete,unlist(tesseract_vec_100[[1]])) > 0)]
+  delete_candidate <- vec_word_delete[!is.na(match(vec_word_delete,unlist(tesseract_vec_100)) > 0)]
   
   # substitution
   list_word <- list(NA)
@@ -26,7 +26,7 @@ candidate <- function(my_word){
     stri_sub(list_word[[j]],j,j) <- letters
   }
   vec_word_substitute <- unlist(list_word)
-  substitute_candidate <- vec_word_substitute[!is.na(match(vec_word_substitute,unlist(tesseract_vec_100[[1]])) > 0)]
+  substitute_candidate <- vec_word_substitute[!is.na(match(vec_word_substitute,unlist(tesseract_vec_100)) > 0)]
   
   # insertion
   list_word <- list(NA)
@@ -35,7 +35,7 @@ candidate <- function(my_word){
     stri_sub(list_word[[i]],i,i) <- ""
   }
   vec_word_insert <- unlist(list_word)
-  insert_candidate <- vec_word_insert[!is.na(match(vec_word_insert,unlist(tesseract_vec_100[[1]])) > 0)]
+  insert_candidate <- vec_word_insert[!is.na(match(vec_word_insert,unlist(tesseract_vec_100)) > 0)]
   
   # reversal
   list_word <- list(NA)
@@ -45,13 +45,13 @@ candidate <- function(my_word){
     stri_sub(list_word[[i]],i+1,i+1) <- stri_sub(my_word,i,i)
   }
   vec_word_reversal <- unlist(list_word)
-  reversal_candidate <- vec_word_reversal[!is.na(match(vec_word_reversal,unlist(tesseract_vec_100[[1]])) > 0)]
+  reversal_candidate <- vec_word_reversal[!is.na(match(vec_word_reversal,unlist(tesseract_vec_100)) > 0)]
   
   
-  list_candidate[[1]] <- delete_candidate
-  list_candidate[[2]] <- substitute_candidate
-  list_candidate[[3]] <- insert_candidate
-  list_candidate[[4]] <- reversal_candidate
+  list_candidate[[1]] <- unique(delete_candidate)
+  list_candidate[[2]] <- unique(substitute_candidate)
+  list_candidate[[3]] <- unique(insert_candidate)
+  list_candidate[[4]] <- unique(reversal_candidate)
   
   return(list_candidate)
 }
